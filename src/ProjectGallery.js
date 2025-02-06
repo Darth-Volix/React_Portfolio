ab8ec0a674765e7829be30258f36e95f670e6adb
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2025-02-05 17:25:12 -0700

    refactor: update ProjectGallery to remove id from project objects

--- src/ProjectGallery.js
+++ src/ProjectGallery.js
@@ -4,9 +4,9 @@ import GlacierSite from './images/glacier_site.png';
 import NoteTaker from './images/typescript_notetaker.png';
 
 const projects = [
-  {id: 1, title: 'ChronoPlan', image: ChronoPlan, content: "Chronoplan is a website that allows a user to track and add goals and events to the calendar via local storage. It also uses an API that enables the user to choose which country's holidays they would like to have displayed on their calendar. Everything for this website was written with vanilla HTML/CSS/JavaScript."},
-  {id: 2, title: 'Glacier NPS Copycat Site', image: GlacierSite, content: "To learn how to use APIs and more advanced vanilla JS, I created a website that very closely mirrors the real website for Glacier National Park, even using the official NPS API. Everything for this website was written with vanilla HTML/CSS/JavaScript."},
-  {id: 3, title: 'TypeScript Notes App', image: NoteTaker, content: "This application was one created to help me learn TypeScript. It is completely terminal based and allows a user to create, edit, and organize notes into folders. The principles of OOP were implemented in this program and the code is open to the public on GitHub."}
+  {title: 'ChronoPlan', image: ChronoPlan, content: "Chronoplan is a website that allows a user to track and add goals and events to the calendar via local storage. It also uses an API that enables the user to choose which country's holidays they would like to have displayed on their calendar. Everything for this website was written with vanilla HTML/CSS/JavaScript."},
+  {title: 'Glacier NPS Copycat Site', image: GlacierSite, content: "To learn how to use APIs and more advanced vanilla JS, I created a website that very closely mirrors the real website for Glacier National Park, even using the official NPS API. Everything for this website was written with vanilla HTML/CSS/JavaScript."},
+  {title: 'TypeScript Notes App', image: NoteTaker, content: "This application was one created to help me learn TypeScript. It is completely terminal based and allows a user to create, edit, and organize notes into folders. The principles of OOP were implemented in this program and the code is open to the public on GitHub."}
 ];
 
 function ProjectGallery() {
