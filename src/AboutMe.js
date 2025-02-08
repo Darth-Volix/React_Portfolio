be1354ea50224a7d1112758a1ac889c76b285aed
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2025-02-08 09:46:59 -0700

    style: restructure About Me section layout; enhance styling for headshot and introduction

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,16 +5,18 @@ function AboutMe(props) {
     return (
         <section id='about-me-card' className='content-card'>
             <h1>About Me</h1>
-            <img id="about-me-photo" src={props.src} alt="personal headshot"></img>
-            <p>
-                Hello! I'm Nicholas Wilkins. Born and bred in the heart of Salt Lake City, Utah, I had the unique experience of growing up as the third of eight siblings. My journey has since led me to Rexburg, Idaho, where my wife and I have called home for nearly four wonderful years.
+            <div id='headshot-intro'>
+                <img id="about-me-photo" src={props.src} alt="personal headshot"></img>
+                <p>
+                    Hello! I'm Nicholas Wilkins. Born and bred in the heart of Salt Lake City, Utah, I had the unique experience of growing up as the third of eight siblings. My journey has since led me to Rexburg, Idaho, where my wife and I have called home for nearly four wonderful years.
 
-                In June 2024, we welcomed our little bundle of joy, Archer, who has brought an endless supply of sunshine into our lives. Currently, I'm pursuing my passion for technology as a software engineering major at BYU-I.
+                    In June 2024, we welcomed our little bundle of joy, Archer, who has brought an endless supply of sunshine into our lives. Currently, I'm pursuing my passion for technology as a software engineering major at BYU-I.
 
-                When I'm not immersed in coding, you can find me fervently cheering for my favorite football teams, indulging in epic Star Wars marathons, diving into the latest tech trends, or passionately singing along to Taylor Swift's hits—yes, I'm a proud Swiftie!
+                    When I'm not immersed in coding, you can find me fervently cheering for my favorite football teams, indulging in epic Star Wars marathons, diving into the latest tech trends, or passionately singing along to Taylor Swift's hits—yes, I'm a proud Swiftie!
 
-                To learn more about my education and accomplishments, choose one of the options below!
-            </p>
+                    To learn more about my education and accomplishments, choose one of the options below!
+                </p>
+            </div>
             <AboutMeGallery />
         </section>
     );
